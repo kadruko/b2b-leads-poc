0049c806be89b55f4d9506589fe4032c7cf59846
I518046 <konrad.schewe@sap.com>
2025-01-21 12:04:59 +0100

    get client ip

--- app/routes/events.tsx
+++ app/routes/events.tsx
@@ -1,7 +1,13 @@
 import { ActionFunctionArgs } from "@remix-run/node";
+import { getClientIPAddress } from "remix-utils/get-client-ip-address";
 
-export const action = async ({ request }: ActionFunctionArgs) => {
+export const action = async ({ request }: ActionFunctionArgs & {
+    request: {socket: any}
+}) => {
     console.log('EVENT_RECEIVED');
+
+    const ipAddress = getClientIPAddress(request);
+    console.log('CLIENT_IP', ipAddress);
 
     const body = await request.text();
     const event = JSON.parse(body);
