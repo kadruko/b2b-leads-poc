8bdde343be626e9b1daa8620947a47ac6e16f3a9
I518046 <konrad.schewe@sap.com>
2025-02-05 17:17:59 +0100

    deployment files

--- app/routes/events.tsx
+++ app/routes/events.tsx
@@ -12,7 +12,7 @@ export const action = async ({ request }: ActionFunctionArgs & {
     const body = await request.text();
     const event = JSON.parse(body);
 
-    console.log('PAYLOAD', event);    
+    console.log('PAYLOAD', event);
 
     return new Response(body, {
         status: 200,
