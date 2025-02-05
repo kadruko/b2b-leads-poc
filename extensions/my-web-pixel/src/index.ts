e395be31441466ab2d67c3e796e6d3be3507d0a5
I518046 <konrad.schewe@sap.com>
2025-02-05 22:43:25 +0100

    .

--- extensions/my-web-pixel/src/index.ts
+++ extensions/my-web-pixel/src/index.ts
@@ -1,18 +1,19 @@
-import { register } from "@shopify/web-pixels-extension";
+import { register } from '@shopify/web-pixels-extension';
 import axios from 'axios';
 
 register(({ analytics, browser, init, settings }) => {
-    // Bootstrap and insert pixel script tag here
+  // Bootstrap and insert pixel script tag here
 
-    // Sample subscribe to page view
-    analytics.subscribe('page_viewed', (event) => {
-      console.log('Page viewed', event);
+  // Sample subscribe to page view
+  analytics.subscribe('page_viewed', (event) => {
+    console.log('Page viewed', event);
 
-      const url = 'https://localhost:3000/events';
-      axios.post(url, event, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+    // const url = 'https://localhost:3000/events';
+    const url = 'https://shopify-b2b-leads.glowsoft.de/events';
+    axios.post(url, event, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
+  });
 });
